bdbb363e54787a96751d9fa25fb851295d300449
Mei Wong <fongmeiw@gmail.com>
2019-10-09 15:34:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(string)
 
   array = string.split(" ")
   array.each do |s|
-    reversed_string.unshift(s)
+    reversed_string.push(s.reverse) 
   end
   
-  reversed_string
+  reversed_string.join(" ")
 end
