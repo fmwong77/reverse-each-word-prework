ef18e75db0414ad39c66c595f805ac15130a5995
Mei Wong <fongmeiw@gmail.com>
2019-10-09 15:40:05 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  reversed_string = []
+
+  array = string.split(" ")
+  array.each do |s|
+    reversed_string.push(s.reverse) 
+  end
+  
+  reversed_string.join(" ")
+  
+  reversed_string = []
+  
+  array.collect do |s|
+    reversed_string.push(s.reverse) 
+  end
+  reversed_string.join(" ")
+end
